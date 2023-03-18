825f55fdcb9505418b9591f7f330f44aab7fe81f
KatanES <businesskatan.es@gmail.com>
2023-03-18 21:04:48 +0100

    card-set-wrapper

---
+++ src/main.js
@@ -0,0 +1,14 @@
+//ініціалізація Swiper
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  //безкінцево
+  loop: true,
+});
